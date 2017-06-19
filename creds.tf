d7cf486d74c47999155961654e7092002c9bc458
kavyaspandanamandadi <spandana.kavya415@gmail.com>
2017-06-19 04:49:28 -0400

    Update creds.tf

--- creds.tf
+++ creds.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIAIS2T3OE5TDISGNCA"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 
     region = "us-east-1"
 }
